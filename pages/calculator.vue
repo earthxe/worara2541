9cd9274a0d81bde01ae8f3b1a21433ed503dcfda
earthxe <earthzamapro@gmail.com>
2023-04-22 22:20:41 +0700

    first commit

---
+++ pages/calculator.vue
@@ -0,0 +1,51 @@
+<template> 
+    <v-app>
+        <v-row align='center' justify='center'>
+         <v-card width="500">
+                 <v-card-text>
+                         <v-row align="center" justify='center'>
+                             <h1>Anwser : {{anwser}}</h1>
+                         </v-row>
+                         <v-row align="center" justify="center">
+
+                         </v-row>
+                     <v-row><v-text-field outlined v-model="v1"></v-text-field>
+                    </v-row> 
+                     <v-row>
+                         <v-text-field outlined v-model="v2"></v-text-field>
+                     </v-row>
+                             <v-row  align="center" justify='center'><v-btn larg color="green" @click="cal()"> button
+                         </v-btn> 
+                      
+                             </v-row>
+                 </v-card-text>       
+         </v-card>   
+      </v-row>
+           
+    </v-app>
+ </template>
+
+<script>
+    export default {
+        data:()=>{
+            return{
+                v1:99,
+                v2:null,
+                anwser:null,
+            }
+        },
+        computed: { //ทำงานตลอดเวลา
+
+        },
+        watch: { 
+
+        },
+        methods: { //สร้าง function
+            cal(){
+                this.anwser = parseInt(this.v1) + parseInt(this.v2)
+                console.log(this.anwser);
+            }
+        }, 
+        created () {},
+    }
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template> 
+   <v-app>
+       <v-row align='center' justify='center'>
+        <v-card width="600">
+                <v-card-text>
+                        <v-row align="center" justify='center'>
+                            <h1> Project </h1>
+                        </v-row>
+                    <v-row><v-text-field outlined label='E-mail'>
+                        </v-text-field></v-row> 
+                    <v-row>
+                        <v-text-field outlined label='password' type="password">
+                        </v-text-field>
+                    </v-row>
+                            <v-row  align="center" justify='center'><v-btn larg color="green" outlined rounded x-large> login 
+                        </v-btn> 
+                     <v-btn  larg color="red" outlined rounded x-large> reset </v-btn>
+                            </v-row>
+                </v-card-text>       
+        </v-card>   
+     </v-row>
+          
+   </v-app>
+</template>
